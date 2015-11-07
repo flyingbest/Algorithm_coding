cff5724ab6dff6303eaa5fee175d45a2d5cc243a
flyingbest <taeyoon4715@gmail.com>
2015-11-07 19:07:54 +0900

    연습

--- List/prac.cpp
+++ List/prac.cpp
@@ -3,33 +3,45 @@ using namespace std;
 
 typedef struct node{
 	int data;
-	struct node *next;
+	struct node* next;
 } Node;
 
-void initNode(Node *head, int n){
+void initNode(Node* head, int n){
 	head->data = n;
 	head->next = NULL;
 }
 
-void addNode(Node *head, int n){
-	Node *newNode = new Node;
+void addNode(Node* head, int n){
+	Node* newNode = new Node;
+	newNode->data = n;
+	newNode->next = NULL;
 	
+	Node* cur = head;
+	while(cur){
+		if(cur->next == NULL){
+			cur->next = newNode;
+			return;
+		}
+		cur = cur->next;
+	}
 }
 
-
-void display(Node *head){
-	Node *list = head;
-	while(list){
-		cout << list->data << " ";
-		list = list->next;
+void display(Node* head){
+	Node* cur = head;
+	while(cur){
+		cout << cur->data << " ";
+		cur = cur->next;
 	}
 	cout << endl;
-}	
+}
 
 int main(){
-	Node *head = new Node;
+	Node* head = new Node;
 	
 	initNode(head, 10);
+	display(head);
+	
+	addNode(head, 20);
 	display(head);
 	
 	return 0;
