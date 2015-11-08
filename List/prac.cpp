da168b1f0959bc96355e15b8f333f98034cc8e0a
flyingbest <taeyoon4715@gmail.com>
2015-11-08 09:40:26 +0900

    오전 연습

--- List/prac.cpp
+++ List/prac.cpp
@@ -4,7 +4,7 @@ using namespace std;
 typedef struct node{
 	int data;
 	struct node* next;
-} Node;
+}	Node;
 
 void initNode(Node* head, int n){
 	head->data = n;
@@ -26,11 +26,47 @@ void addNode(Node* head, int n){
 	}
 }
 
-void display(Node* head){
+void addFront(Node** head, int n){
+	Node* newNode = new Node;
+	newNode->data = n;
+	newNode->next = *head;
+	*head = newNode;
+}
+
+Node* searchNode(Node* head, int n){
 	Node* cur = head;
 	while(cur){
-		cout << cur->data << " ";
+		if(cur->data == n)	return cur;
 		cur = cur->next;
+	}
+	cout << "No Node " << n << " in the list.\n";
+}
+
+bool deleteNode(Node** head, Node* ptrDel){
+	Node* cur = *head;
+	if(ptrDel == *head){
+		*head = cur->next;
+		delete ptrDel;
+		return true;
+	}
+	while(cur){
+		if(cur->next == ptrDel){
+			cur->next = ptrDel->next;
+			delete ptrDel;
+			return true;
+		}
+		cur = cur->next;
+	}
+	return false;
+}
+
+
+
+void display(Node* head){
+	Node* list = head;
+	while(list){
+		cout << list->data << " ";
+		list = list->next;
 	}
 	cout << endl;
 }
@@ -38,11 +74,20 @@ void display(Node* head){
 int main(){
 	Node* head = new Node;
 	
-	initNode(head, 10);
+	initNode(head, 20);
 	display(head);
-	
-	addNode(head, 20);
+	addNode(head, 30);
 	display(head);
-	
+	addNode(head, 40);
+	display(head);
+	addFront(&head, 10);
+	display(head);
+	addFront(&head, 5);
+	display(head);
+	Node* ptrDel = searchNode(head, 7);
+	ptrDel = searchNode(head, 10);
+		
+	display(head);
 	return 0;
 }
+
