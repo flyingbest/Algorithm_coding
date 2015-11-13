2ca8264c613ee328f71f13e8959cf4a153ad550f
flyingbest <taeyoon4715@gmail.com>
2015-11-13 22:47:57 +0900

    rewrite

--- List/test.cpp
+++ List/test.cpp
@@ -4,97 +4,33 @@ using namespace std;
 typedef struct node{
 	int data;
 	struct node* next;
-	struct node* prev;
 } Node;
 
-void printFront(Node* head){
+void printlist(Node* head){
 	Node* list = head;
 	while(list){
 		cout << list->data << " ";
-		list = list->next;
+		list = list->data;
 	}
 	cout << endl;
 }
 
-void printReverse(Node* tail){
-	Node* list = tail;
-	while(list){
-		cout << list->data << " ";
-		list = list->prev;
-	}
-	cout << endl;
-}
-
-void initNode(Node* head, Node** tail, int n){
+void initNode(Node* head, int n){
 	head->data = n;
 	head->next = NULL;
-	head->prev = NULL;
-	*tail = head;
 }
 
-void addNode(Node* head, Node** tail, int n){
+void addNode(Node* head, int n){
 	Node* newNode = new Node;
 	newNode->data = n;
 	newNode->next = NULL;
-
+	
 	Node* cur = head;
 	while(cur){
 		if(cur->next == NULL){
-			cur->next = newNode;
-			newNode->prev = *tail;
-			*tail = newNode;
-			return;
-		}
-		cur = cur->next;
-	}
-}
+			
+	
 
-void addFront(Node** head, int n){
-	Node* newNode = new Node;
-	newNode->data = n;
-	newNode->next = *head;
-	(*head)->prev = newNode;
-	*head = newNode;
-	newNode->prev = NULL;
-}
-
-Node* searchNode(Node* head, int n){
-	Node* cur = head;
-	while(cur){
-		if(cur->data == n){
-			cout << "Node < " << n << " > exist in this list.\n";
-			return cur;
-		}
-		cur = cur->next;
-	}
-	cout << "Node < " << n << " > NOT exist in this list.\n";
-}
-
-bool deleteNode(Node** head, Node** tail, Node* ptrDel){
-	Node* cur = *head;
-	if(ptrDel == *head){
-		*head = cur->next;
-		(*head)->prev = NULL;
-		delete ptrDel;
-		return true;
-	}
-	Node* tmp = NULL;
-	while(cur){
-		if(cur->next == ptrDel){
-			cur->next = ptrDel->next;
-			if(ptrDel->next != NULL){
-				tmp = ptrDel->next;
-				tmp->prev = cur;
-			}else{
-				*tail = cur;
-			}
-			delete ptrDel;
-			return true;
-		}
-		cur = cur->next;
-	}
-	return false;
-}
 
 int main(){
 	Node* head = new Node;
