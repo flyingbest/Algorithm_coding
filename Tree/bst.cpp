37d17c9b4c2988cb08efd72a9e9e4e9de6e39da8
flyingbest <taeyoon4715@gmail.com>
2015-11-24 20:31:19 +0900

    에러 있음

--- Tree/bst.cpp
+++ Tree/bst.cpp
@@ -51,7 +51,28 @@ Node* removeNode(Node* tree, int delData){
 	if(tree == NULL) cout << "해당노드를 찾을 수 없음.\n";
 	else if(tree->data >= delData) tree->left = removeNode(tree->left, delData);
 	else if(tree->data < delData) tree->right = removeNode(tree->right, delData);
-	else
+	else{
+		if(tree->left != NULL && tree->right != NULL){
+			tmpNode = findMinNode(tree->right);
+			tree->data = tmpNode->data;
+			tree->right = removeNode(tree->right, tmpNode->data);
+		}
+		else{
+			tmpNode = tree;
+			if(tree->left == NULL) tree = tree->right;
+			else if(tree->right == NULL) tree = tree->left;
+			delete tmpNode;
+		}
+	}
+	return tree;
+}
+
+void printTree(Node* tree){
+	if(tree == NULL) return;
+	printTree(tree->left);
+	cout << tree->data << " ";
+	printTree(tree->right);
+}
 
 int main(){
 	Node* root = NULL;
@@ -64,7 +85,22 @@ int main(){
 	insertNode(root, createNode(25));
 	insertNode(root, createNode(7));
 
+	Node* findNode;
+	int input;
 
+	while(true){
+		cin >> input;
+		findNode = searchNode(root, input);
+		
+		if(findNode != NULL){
+			cout << "해당 노드를 찾았습니다! 노드를 제거합니다.\n";
+			removeNode(root, input);
+			cout << "현재 트리 출력 : ";
+			printTree(root); cout << endl;
+		}
+		else
+			cout << "노드를 찾을 수 없습니다.\n";
+	}
 	return 0;
 }
 
