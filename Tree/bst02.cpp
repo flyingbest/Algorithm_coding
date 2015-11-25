d59a6394b2074c2c897342f8d0ffb6fe064f1c3e
flyingbest <taeyoon4715@gmail.com>
2015-11-25 20:49:25 +0900

    bst 연습 - deleting 아직 못함

--- Tree/bst02.cpp
+++ Tree/bst02.cpp
@@ -38,20 +38,109 @@ bool searchNode(BstNode* root, int data){
 	else return searchNode(root->right, data);
 }
 
+int findMin(BstNode* root){
+	if(root == NULL){
+		cout << "Error : Tree is empty\n";
+		return -1;
+	}
+	/*
+	BstNode* cur = root;
+	while(cur->left != NULL){
+		cur = cur->left;
+	}
+	return cur->data;
+	*/
+	// second method.
+	if(root->left == NULL){
+		return root->data;
+	}
+	// search in left subtree.
+	return findMin(root->left);
+}
+
+int findMax(BstNode* root){
+	if(root == NULL){
+		cout << "Error : Tree is empty\n";
+		return -1;
+	}
+	else if(root->right == NULL){
+		return root->data;
+	}
+	return findMax(root->right);
+}
+
+/*
+BstNode* deleteNode(BstNode* root, int deldata){
+	if(root == NULL) return root;
+	else if(deldata < root->data) root->left = deleteNode(root-left, deldata);
+	else if(deldata > root->data) root->right = deleteNode(root->right, deldata);
+	else{
+		//case 1 : No child
+		if(root->left == NULL && root->right == NULL){
+			delete root;
+			root = NULL;
+		}
+		//case 2 : One child
+		else if(root->left == NULL){
+			BstNode* tmp = root;
+			root = root->right;
+			delete tmp;
+		}
+		else if(root->right == NULL){
+			BstNode* tmp = root;
+			root = root->left;
+			delete tmp;
+		}
+		//case 3 : 2 children
+		else{
+			BstNode* tmp;
+			tmp->data = findMin(root->right);
+			root->data = tmp->data;
+			root->right = deleteNode(root->right, tmp ->data);
+		}
+	}	
+	return root;
+}
+*/
+
+void printInorder(BstNode* root){
+	if(root != NULL){
+		printInorder(root->left);
+		cout << root->data << " ";
+		printInorder(root->right);
+	}
+}
+
 int main(){
 	BstNode* root = NULL;	// creating an empty tree
-	root = insertNode(root, 15);	root = insertNode(root, 10);
-	root = insertNode(root, 20);	root = insertNode(root, 25);
-	root = insertNode(root, 5); 	root = insertNode(root, 12);
+	root = insertNode(root, 20);	root = insertNode(root, 30);
+	root = insertNode(root, 25);	root = insertNode(root, 10);
+	root = insertNode(root, 15); 	root = insertNode(root, 5);	root = insertNode(root, 35);
+	
+	printInorder(root);
 
+	
 	int num;
-	while(true){
-		cout << "Enter number be searched\n";
-		cin >> num;
-		
-		if(searchNode(root, num) == true) cout << "Found!\n";
-		else cout << "Not Found!\n";
-	}
+	cout << "Enter number be searched\n";
+	cin >> num;
+	
+	if(searchNode(root, num) == true) cout << "Found!\n";
+	else cout << "Not Found!\n";
+
+	printInorder(root);
+
+	int min;
+	cout << "let's find Min!\n";
+	min = findMin(root);
+	cout << "min : " << min << endl;
+	
+	int max;
+	cout << "let's find Max!\n";
+	max = findMax(root);
+	cout << "max : " << max << endl;
+
+	printInorder(root);	
+	
 	return 0;
 }
 
